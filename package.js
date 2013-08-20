d651630b43d611778e89fbd4824f4fd9a629ee7e
digilord <digilord@me.com>
2013-08-20 10:29:40 -0700

    Added an api.use line for two dependencies.

--- package.js
+++ package.js
@@ -3,6 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
+  api.use(['underscore', 'underscore-string-latest'], ['client', 'server']);
   api.add_files(['lib/ev.js', 'lib/server.js', 'lib/stream_permission.js'], 'server');
   api.add_files(['lib/ev.js', 'lib/client.js'], 'client');
 });
